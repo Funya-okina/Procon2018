ef4591cd0ecc11487866d11287a05f929ced8663
strvworks <strv13570@gmail.com>
2018-10-27 20:20:13 +0900

    fix reject

--- crossing_cell_server.py
+++ crossing_cell_server.py
@@ -153,11 +153,20 @@ class Server(object):
         elif client_update['from'] == "B":
             self.turnBehavior[1] = True
             self.client_update_dict["B"] = client_update
+
         if all(self.turnBehavior):
             self.turnBehavior = [False, False]
-            self.webUi.setTurnConfirmView(True)
+            self.nextTurn()
+            # self.webUi.setTurnConfirmView(True)
 
     def nextTurn(self):
+        for i in range(2):
+            new_loc_a = self.client_update_dict["A"]["agent_location"][i]
+            if new_loc_a in self.client_update_dict["B"]["agent_location"]:
+                self.client_update_dict["A"]["agent_location"][i] = self.board.current_agent_cells_a[i]
+                index_b = self.client_update_dict["B"]["agent_location"].index(new_loc_a)
+                self.client_update_dict["B"]["agent_location"][index_b] = self.board.current_agent_cells_b[index_b]
+
         self.board.setCurrentAgentLocations(self.client_update_dict["A"]["agent_location"], "A")
         self.board.setCurrentAgentLocations(self.client_update_dict["B"]["agent_location"], "B")
         for x in self.remove_tiles:
